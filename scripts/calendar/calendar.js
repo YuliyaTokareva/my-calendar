0ece0c9542f5399dfa8844c8dd2b26dc1ec47393
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-04-18 00:24:17 +0200

    add getWeek code

--- scripts/calendar/calendar.js
+++ scripts/calendar/calendar.js
@@ -4,14 +4,33 @@
 // import { createNumbersArray } from '../common/createNumbersArray.js';
 
 const generateDay = () => {
-  // функция должна сгенерировать и вернуть разметку дня в виде строки
-  // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+    // функция должна сгенерировать и вернуть разметку дня в виде строки
+    // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+    return createNumbersArray(1, 24)
+        .map(
+            (timeNumber) => `
+          <div 
+          class="calendar__time-slot" 
+          data-time="${timeNumber}"></div>`
+        )
+        .join("");
 };
 
 export const renderWeek = () => {
-  // функция должна сгенерировать разметку недели в виде строки и вставить ее на страницу (в .calendar__week)
-  // разметка недели состоит из 7 дней (.calendar__day) отображаемой недели
-  // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
-  // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
-  // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+    // функция должна сгенерировать разметку недели в виде строки и вставить ее на страницу (в .calendar__week)
+    // разметка недели состоит из 7 дней (.calendar__day) отображаемой недели
+    // массив дней, которые нужно отобразить, считаем ф-цией generateWeekRange на основе displayedWeekStart из storage
+    // каждый день должен содержать в дата атрибуте порядковый номер дня в месяце
+    // после того, как отрисовали всю сетку для отображаемой недели, нужно отобразить события этой недели с помощью renderEvents
+    const findWeek = document.querySelector(".calendar__week");
+    const getTime = generateDay;
+    const getWeek = generateWeekRange()
+        .map(
+            (dayNumber) => `
+           <div 
+            class="calendar__day" 
+            data-day="${dayNumber}">"${getTime}"</div>`
+        )
+        .join("");
+    findWeek.innerHTML = getWeek;
 };
